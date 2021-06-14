ca650cb7c35a1996f122295e5f3105f8ab0e0c2e
Garren Goudy <garren.goudy@yahoo.com>
2021-06-14 14:01:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,14 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
+  if int % 3 %% 5 == 0
+    "FizzBuzz"
   
   elsif int % 5 == 0
     "Buzz"
     
-  elsif int % 3 && 5 == 0 
-    "FizzBuzz"
+  elsif int % 3 == 0 
+    "Fizz"
     
   end
 end
